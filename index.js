e7afc37ce46f9064d074045440ff9402d501a51e
zhangzhenfei <fei@sadais.com>
2020-07-17 09:33:06 +0800

    feat: 优化piui install console.log

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import piTools from './tools'
 import { getConfig, setConfig } from './config'
 
+const logStyle = 'color:#ff6a00;font-size:20px;font-weight:500;'
+
 const install = (Vue, config = {}) => {
   const piConfig = piTools.object.mergeDeep(getConfig(), config)
   setConfig(piConfig)
-  console.log('piui 已安装，使用配置为：', piConfig)
-  console.log('piui tools已挂载，使用方法：this.$piTools.', piTools)
+  console.log('%c欢迎使用piui，官网地址：https://www.sadais.com', logStyle)
+  console.log('%cpiui 已安装，使用配置为：', logStyle, piConfig)
+  console.log('%cpiui tools 已挂载：', logStyle, piTools)
+  console.log('%cpiui tools 模板使用方法 {{ $piTools.xxx }}', logStyle)
+  console.log('%cpiui tools js使用方法：this.$piTools.xxx', logStyle)
   Vue.prototype.$piConfig = piConfig
   Vue.prototype.$piTools = piTools
 }
