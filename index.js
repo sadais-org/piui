d91bf43ae094063927a32a2efddd028c3ef8454d
zhangzhenfei <fei@sadais.com>
2020-09-22 19:39:26 +0800

    feat: 优化打印日志

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const logStyle = 'color:#ff6a00;font-size:20px;font-weight:500;'
 const install = (Vue, config = {}) => {
   const piConfig = tools.lang.mergeDeep(getConfig(), config)
   setConfig(piConfig)
-  console.log('%c欢迎使用piui，官网地址：https://www.sadais.com', logStyle)
-  console.log('%cpiui 已安装，使用配置为：', logStyle, piConfig)
-  console.log('%cpiui tools 已挂载：', logStyle, tools)
-  console.log('%cpiui tools 模板使用方法 {{ $pi.xxx }}', logStyle)
-  console.log('%cpiui tools js使用方法：this.$pi.xxx', logStyle)
   Vue.prototype.$piConfig = piConfig
   Vue.prototype.$pi = tools
   Vue.prototype.$toast = tools.toast.info
   Vue.prototype.$loading = tools.toast.loading
   Vue.prototype.$hideLoading = tools.toast.hideLoading
+
+  console.log('%c欢迎使用piui，官网地址：https://www.sadais.com', logStyle)
+  console.log('%cpiui 已安装，使用配置为：', logStyle, piConfig)
+  console.log('%cpiui tools 已挂载：', logStyle, tools)
+  console.log('%cpiui tools 模板使用方法 {{ $pi.xxx }}', logStyle)
+  console.log('%cpiui tools js使用方法：this.$pi.xxx', logStyle)
 }
 
 export default install
