3ffcbdc36bc6be8b6391f6beae4ac82aa4dea1b7
yangbo <yangbo@sadais.com>
2020-10-21 10:26:05 +0800

    fix: 修复pi-modal点击事件bug

--- components/pi-modal/index.vue
+++ components/pi-modal/index.vue
@@ -44,7 +44,7 @@
               :color="cancelColor"
               :bg-color="cancelBgColor"
               custom-class="pi-w-100P"
-              @tap="handleCancel"
+              @click="handleCancel"
             >
               {{ cancelText }}
             </pi-button>
@@ -55,7 +55,7 @@
               :color="confirmColor"
               :bg-color="confirmBgColor"
               custom-class="pi-w-100P"
-              @tap="handleConfirm"
+              @click="handleConfirm"
             >
               {{ confirmText }}
             </pi-button>
