1d6d00598ac50ed4ea19e25d6c4144a0cb9e1622
zhangzhenfei <fei@sadais.com>
2020-11-14 17:47:55 +0800

    feat: pi-img加上红点和徽章

--- components/pi-img/index.vue
+++ components/pi-img/index.vue
@@ -1,36 +1,42 @@
 <template>
-  <view
-    class="pi-img pi-w-100P pi-align-center"
-    :style="[imageStyle, customStyle]"
-    :class="[customClass]"
-    @tap="handleButtonTap"
-  >
-    <image
-      :src="src"
-      :mode="mode"
-      :lazy-load="lazyLoad"
-      :style="[imageStyle]"
-      @error="handleError"
-      @load="handleLoad"
-    />
-    <view class="pi-abso-center">
-      <!-- 加载中 -->
-      <template v-if="loading">
-        <slot v-if="$slots.loading" name="loading" />
-        <pi-loading
-          v-else
-          :show="loading"
-          :type="loadingType"
-          :color="loadingColor"
-          :size="loadingSize"
-        />
-      </template>
-      <!-- 加载失败 -->
-      <template v-if="error">
-        <slot v-if="$slots.error" name="error" />
-        <view class="pi-icon-pic" />
-      </template>
+  <view class="pi-img" :style="[rectStyle]">
+    <view
+      class="pi-align-center"
+      :style="[rectStyle, imageStyle, customStyle]"
+      :class="[customClass]"
+      @tap="handleButtonTap"
+    >
+      <image
+        :src="src"
+        :mode="mode"
+        :lazy-load="lazyLoad"
+        :style="[rectStyle, imageStyle]"
+        @error="handleError"
+        @load="handleLoad"
+      />
+      <view class="pi-abso-center">
+        <!-- 加载中 -->
+        <template v-if="loading">
+          <slot v-if="$slots.loading" name="loading" />
+          <pi-loading
+            v-else
+            :show="loading"
+            :type="loadingType"
+            :color="loadingColor"
+            :size="loadingSize"
+          />
+        </template>
+        <!-- 加载失败 -->
+        <template v-if="error">
+          <slot v-if="$slots.error" name="error" />
+          <view class="pi-icon-pic" />
+        </template>
+      </view>
     </view>
+    <view v-if="badge || dot" class="img-badge" :class="{ dot }" :style="[badgeStyle]">
+      {{ badge }}
+    </view>
+    <view v-if="dot" />
   </view>
 </template>
 
@@ -81,6 +87,22 @@ export default {
     height: {
       type: [String, Number],
       default: img.height
+    },
+    // 是否显示图标右上角小红点，默认值 false
+    dot: {
+      type: Boolean,
+      default: img.dot
+    },
+    // 小红点的半径
+    dotRadius: {
+      type: [String, Number],
+      // 16rpx
+      default: img.dotRadius
+    },
+    // 图标右上角徽标的内容
+    badge: {
+      type: [String, Number],
+      default: img.badge
     },
     // 图片形状，round-圆形，square-方形（默认值：'square'）
     shape: {
@@ -148,11 +170,26 @@ export default {
     }
   },
   computed: {
+    getDotRadius() {
+      return this.$pi.common.addUnit(this.dotRadius)
+    },
+    rectStyle() {
+      return {
+        width: this.$pi.common.addUnit(this.width),
+        height: this.$pi.common.addUnit(this.height)
+      }
+    },
+    badgeStyle() {
+      const style = {}
+      if (this.dot) {
+        style.width = this.getDotRadius
+        style.height = this.getDotRadius
+      }
+      return style
+    },
     imageStyle() {
       return {
         overflow: 'hidden',
-        width: this.$pi.common.addUnit(this.width),
-        height: this.$pi.common.addUnit(this.height),
         borderRadius: this.shape === 'round' ? '50%' : this.$pi.common.addUnit(this.borderRadius)
       }
     }
@@ -177,5 +214,28 @@ export default {
 <style lang="scss" scoped>
 .pi-img {
   position: relative;
+  .img-badge {
+    position: absolute;
+    top: 0;
+    right: 0;
+    box-sizing: border-box;
+    min-width: 16px;
+    padding: 0 3px;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 1.2;
+    color: #ffffff;
+    text-align: center;
+    background-color: #ee0a24;
+    border: 1px solid #ffffff;
+    border-radius: 16px;
+    transform: translate(50%, -50%);
+    transform-origin: 100%;
+    &.dot {
+      min-width: 0;
+      background-color: #ee0a24;
+      border-radius: 100%;
+    }
+  }
 }
 </style>
--- config/img.js
+++ config/img.js
@@ -5,6 +5,9 @@ export default {
   mode: 'widthFix', // 裁剪模式（默认值：'widthFix'）
   width: '100%', // 宽度（默认值：'100%'）
   height: 'auto', // 高度（默认值：'auto'）
+  dot: false, // 是否显示图标右上角小红点，默认值 false
+  dotRadius: 16, // 小红点的半径
+  badge: '', // 图标右上角徽标的内容
   shape: 'square', // 图片形状，round-圆形，square-方形（默认值：'square'）
   borderRadius: 0, // shape为square的时候设置（默认值：'0rpx'）
   lazyLoad: true, // 是否懒加载（默认值：'true'）
