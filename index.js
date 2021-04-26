50abde89947c73b6bed00f969929719e5e182e61
zhangzhenfei <fei@sadais.com>
2021-04-26 11:08:15 +0800

    :bug: fix: 修复构建报错

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ const install = function(Vue, config = {}) {
   window.addEventListener('resize', setVh)
   // #endif
 
-  console.group('piui')
+  console.group && console.group('piui')
 
   console.log('%cpiui 已安装，教程：https://github.com/sadais-org/piui', logStyle)
   console.log('%cpiui 组件全局配置：', logStyle, piConfig)
   console.log('%cpiui tools 已挂载：', logStyle, this)
 
-  console.groupEnd('piui')
+  console.groupEnd && console.groupEnd('piui')
 }
 
 export default {
