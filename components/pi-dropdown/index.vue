ffcc983ae689874ae6fa2e997458840546f1924c
linbaiquan <linbaiquan@sadais.com>
2021-10-23 17:08:14 +0800

    :bug: fix: 删除没用样式

--- components/pi-dropdown/index.vue
+++ components/pi-dropdown/index.vue
@@ -75,7 +75,6 @@
         show ? getMask.animationShow : getMask.animationHide,
         getMask.hideTabBar ? 'include-tabbar' : 'exclude-tabbar'
       ]"
-      style=" top: 0; left: 0; z-index: 1; width: 100%; height: 100%; background: #000000; opacity: 0.4;"
       :style="[
         {
           'zIndex': 1,
